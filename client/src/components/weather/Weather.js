eb6ee7f49543acd6884003aecb1d2b1d3ec0962d
randyly8 <RaNdYlY8@gmail.com>
2020-12-03 19:46:41 -0600

    Weather api init

--- client/src/components/weather/Weather.js
+++ client/src/components/weather/Weather.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Weather() {
+    const [weather, setWeather] = useState([])
+    const [city, setCity] = useState("")
+    const [state, setState] = useState("")
+    const [country, setCountry] = useState("")
+
+    useEffect(() => {
+        fetch(`/api/weather/${city},${state},${country}`)
+            .then(res => res.json())
+            .then(data => setWeather(data))
+    }, [city, state, country])
+
+    useEffect(() => {
+        console.log('Weather data fetched..', weather)
+    }, [weather])
+
+    const updateLocation = () => {
+        setCity("London")
+        setState("")
+        setCountry("")
+    }
+
     return (
         <div>
-            
+            {/* <input onChange={updateLocation}></input> */}
+            {/* <button onClick={updateLocation}></button>      */}
         </div>
     );
 }
--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const https = require('https');
+const api = require('./apiKey')
 
 const app = express();
 
-/// change this
-app.get('/', (req, res) => {
-    const customers = [
-        {id: 1, firstName: 'John', lastName: 'Doe'},
-        {id: 2, firstName: 'Jane', lastName: 'Smith'},
-        {id: 3, firstName: 'Kira', lastName: 'Light'}
-    ];
+app.get('/api/weather/:location', (req, res) => {   
+    https.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.params.location}&appid=${api.weatherApi}`, (resp) => {
+        let data = '';
 
-    res.json(customers);
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            console.log(JSON.parse(data));
+            res.json(data);
+        });
+    }).on("error", (err) => { 
+        console.log("Error: " + err.message);
+    });
 })
 
-const port = 5000;
-
+const port = 5000; 
+ 
 app.listen(port, () => console.log(`Sever started on port ${port}`));
